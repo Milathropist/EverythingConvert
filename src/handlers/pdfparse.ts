b940aee7bbcf9c443429ff668a8d78b2b7038718
Mila <milenakawecka13@gmail.com>
2026-06-28 17:56:39 +0200

    wa

--- src/handlers/pdfparse.ts
+++ src/handlers/pdfparse.ts
@@ -4,7 +4,76 @@ import type { FileData, FileFormat, FormatHandler } from "../FormatHandler.ts";
 import CommonFormats from "src/CommonFormats.ts";
 import { PDFParse } from 'pdf-parse';
 import { appAssetUrl } from "../assetUrl.ts";
+import JSZip from "jszip";
 
+function escapeXml (value: string): string {
+  return value
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll("\"", "&quot;")
+    .replaceAll("'", "&apos;");
+}
+
+async function textToDocx (text: string): Promise<Uint8Array> {
+  const zip = new JSZip();
+  const paragraphs = text
+    .replace(/\r\n/g, "\n")
+    .replace(/\r/g, "\n")
+    .split("\n");
+  const paragraphXml = paragraphs.map(paragraph => {
+    const escaped = escapeXml(paragraph);
+    if (!escaped) return "<w:p/>";
+    return `<w:p><w:r><w:t xml:space="preserve">${escaped}</w:t></w:r></w:p>`;
+  }).join("");
+
+  zip.file("[Content_Types].xml", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+  <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
+  <Default Extension="xml" ContentType="application/xml"/>
+  <Override PartName="/word/document.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"/>
+  <Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/>
+  <Override PartName="/docProps/app.xml" ContentType="application/vnd.openxmlformats-officedocument.extended-properties+xml"/>
+</Types>`);
+
+  zip.folder("_rels")!.file(".rels", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="word/document.xml"/>
+  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties" Target="docProps/core.xml"/>
+  <Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties" Target="docProps/app.xml"/>
+</Relationships>`);
+
+  zip.folder("docProps")!.file("core.xml", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <dc:title>Converted PDF</dc:title>
+  <dc:creator>Everything Convert</dc:creator>
+  <cp:lastModifiedBy>Everything Convert</cp:lastModifiedBy>
+</cp:coreProperties>`);
+
+  zip.folder("docProps")!.file("app.xml", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+  <Application>Everything Convert</Application>
+</Properties>`);
+
+  zip.folder("word")!.file("document.xml", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<w:document xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
+  <w:body>
+    ${paragraphXml}
+    <w:sectPr>
+      <w:pgSz w:w="12240" w:h="15840"/>
+      <w:pgMar w:top="1440" w:right="1440" w:bottom="1440" w:left="1440" w:header="720" w:footer="720" w:gutter="0"/>
+    </w:sectPr>
+  </w:body>
+</w:document>`);
+  zip.folder("word")!.folder("_rels")!.file("document.xml.rels", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"/>`);
+
+  return await zip.generateAsync({
+    type: "uint8array",
+    compression: "DEFLATE",
+    compressionOptions: { level: 9 }
+  });
+}
 
 class pdfparseHandler implements FormatHandler {
 
@@ -12,6 +81,7 @@ class pdfparseHandler implements FormatHandler {
   public supportedFormats?: FileFormat[] = [
     CommonFormats.PDF.builder("pdf").allowFrom(),
     CommonFormats.TEXT.builder("txt").allowTo(),
+    CommonFormats.DOCX.builder("docx").allowTo(),
   ];
   public ready: boolean = false;
 
@@ -32,9 +102,18 @@ class pdfparseHandler implements FormatHandler {
       const text = await parser.getText();
       await parser.destroy();
 
+      const baseName = inputFile.name.replace(/\.pdf$/i, "");
+      if (outputFormat.internal === "docx") {
+        outputFiles.push({
+          bytes: await textToDocx(text.text),
+          name: `${baseName}.docx`,
+        });
+        continue;
+      }
+
       outputFiles.push({
         bytes: new TextEncoder().encode(text.text),
-        name: inputFile.name.replace(/\.pdf$/i, ".txt"),
+        name: `${baseName}.txt`,
       });
     }
 
